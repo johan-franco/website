1abd04d53d17982905075c8c418a5bbc124a322b
Johan Franco <johan.francocampos@gmail.com>
2024-01-02 09:59:09 -0500

    attempt to fix

--- javascript/WebvilleCinema/cinema.js
+++ javascript/WebvilleCinema/cinema.js
@@ -26,7 +26,7 @@ function init() {
             var showing = document.createElement(td);
             showing.innerHTML = this.getNextShowing();
             row.append(showing);
-        }
+        };
     }
 
     var banzaiMovie= new Movie("Buckaroo Banzai", "Cult Classic", 5, ["1:00pm", "5:00pm", "7:00pm", "11:00pm"]);
