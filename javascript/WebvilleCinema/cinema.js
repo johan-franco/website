3fc81dbdc787990d96c6d4d65ec706cceeedbcd3
Johan Franco <johan.francocampos@gmail.com>
2024-01-02 22:59:00 -0500

    attempt to fix

--- javascript/WebvilleCinema/cinema.js
+++ javascript/WebvilleCinema/cinema.js
@@ -1,5 +1,13 @@
 window.onload = init;
 
+function getTimeFromString(timeString) {
+    var theTime = new Date();
+    var time = timeString.match(/(\d+)(?::(\d\d))?\s*(p?)/);
+    theTime.setHours( parseInt(time[1]) + (time[3] ? 12 : 0) );
+    theTime.setMinutes( parseInt(time[2]) || 0 );
+    return theTime.getTime();
+}
+
 function Movie(title, genre,rating, showtimes, table) {
     this.title = title;
     this.genre = genre;
