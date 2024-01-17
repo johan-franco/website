dbb91d062b25b101a72633e42dd176e719abc5aa
Johan Franco <johan.francocampos@gmail.com>
2024-01-17 18:17:04 -0500

    testing prelim code for memory number game

---
+++ javascript/memory/memory.js
@@ -0,0 +1,11 @@
+function memory() {
+    amount = 1;
+    while(true) {
+        var stage = document.getElementById();
+
+        for(var i = 0; i < amount; i++) {
+            var number = Math.floor(math.random()*9);
+        }
+        stage.innerText(toString(number));
+    }
+}
