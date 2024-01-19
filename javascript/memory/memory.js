632a594e77508480a1b88f4dd35e752b94b61a5e
Johan Franco <johan.francocampos@gmail.com>
2024-01-19 16:54:51 -0500

    added more css

--- javascript/memory/memory.js
+++ javascript/memory/memory.js
@@ -1,10 +1,13 @@
 function memory() {
+    var progress_bar = document.querySelector(".progressinner");
+
     amount = 1;
     while(true) {
-        var stage = document.getElementById();
+        var stage = document.getElementById("number");
 
         for(var i = 0; i < amount; i++) {
             var number = Math.floor(math.random()*9);
+            
         }
         stage.innerText(toString(number));
     }
