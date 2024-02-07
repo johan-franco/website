c693b5f6e10e50fab975247900849d300be886e9
Johan Franco <johan.francocampos@gmail.com>
2024-02-07 15:53:23 -0500

    changed everything to random number

--- javascript/memory/memory.js
+++ javascript/memory/memory.js
@@ -1,14 +1,15 @@
 function memory() {
     var progress_bar = document.querySelector(".progressinner");
 
-    amount = 1;
+    var amount = 0;
 
         var stage = document.getElementById("number");
-
-
-            var number = Math.floor(Math.random()*9);
-            
-
+        var number = Math.floor(Math.random()*9);
+        for(var i = 0; i < amount; i++) {
+            let temp = Math.floor(Math.random()*9);
+            number = toString(number) + toString(temp);
+        }
         stage.innerHTML = number;
         console.log(number);
+
 }
